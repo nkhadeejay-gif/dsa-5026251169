eeeedabffacd3e7c6d6317bf2de1cb10a83aca3e
Noura Khadeeja Yuniarto <nkhadeejay@gmail.com>
2026-09-20 19:52:49 +0700

    Labwork 01 - Prelab

---
+++ src/lw01/prelab/ColourPrint.java
@@ -0,0 +1,26 @@
+package lw01.prelab;
+
+public class ColourPrint extends PrintJob {
+
+    public ColourPrint(String id, int pages) {
+        super(id, pages);
+    }
+
+    @Override
+    public int calculateCharge() {
+        int charge;
+
+        if (pages <= 10) {
+            charge = pages * 1500;
+        } else {
+            charge = (10 * 1500) + ((pages - 10) * 1000);
+        }
+
+        return charge + 2000;
+    }
+
+    @Override
+    public String label() {
+        return "Colour";
+    }
+}
